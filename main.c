00e8c9764e5904b043f0facb4714dd2dc2ab89a9
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-10-29 21:35:05 +0300

    feat: Implemented main for reading from test files

--- main.c
+++ main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "at_parser/parser.h"
 
-int main() {
-    switch(at_command_parse(' ')){
-        case STATE_MACHINE_READY_OK:
-            printf("STATE_MACHINE_READY_OK");
-            break;
-        case STATE_MACHINE_READY_ERROR:
-            printf("STATE_MACHINE_READY_ERROR");
-            break;
-        case STATE_MACHINE_NOT_READY:
-            printf("STATE_MACHINE_NOT_READY");
-            break;
+#define MAXBUFLEN 4096
+
+int main(int argc, char *argv[]) {
+    char *file_path;
+    uint8_t source[MAXBUFLEN + 1];
+    uint16_t newLen;
+    FILE *fp;
+
+    if (argc != 2) {
+        fprintf(stderr, "Error: program expects path to file as argument\n");
+        exit(1);
     }
+
+    file_path = argv[1];
+    printf("%s\n", file_path);
+    if (!(fp = fopen(file_path, "rb"))) {
+        perror(file_path);
+    }
+
+    newLen = fread(source, sizeof(char), MAXBUFLEN, fp);
+    do {
+        if (ferror(fp)) {
+            perror(file_path);
+        } else {
+            source[newLen] = '\0'; /* Just to be safe. */
+            for (int i = 0; i < newLen; i++) {
+                printf("%c", source[i]);
+            }
+        }
+    } while ((newLen = fread(source, sizeof(char), MAXBUFLEN, fp)));
+    fclose(fp);
     return 0;
 }
